ed3458c43f24a572d27f47077848e2c2cc16cdbb
aponte411 <aponted511@gmail.com>
2023-03-23 17:03:15 -0400

    feat: modify transformers bert implementation with adapter module

--- src/model/adapter.py
+++ src/model/adapter.py
@@ -1,14 +1,15 @@
 from torch import nn
 import torch.nn.functional as F
-
+# TODO: switch to yaml config
 from config import *
 
-class Adapter(nn.Module):
+class AdapterModule(nn.Module):
     def __init__(self, in_features: int):
         super().__init__()
         self.ff_down_proj = nn.Linear(in_features, ADAPTER_BOTTLENECK)
         self.ff_up_proj = nn.Linear(ADAPTER_BOTTLENECK, in_features)
 
     def forward(self, X):
+        # Skip connection
         X = X + self.ff_up_proj(F.relu(self.ff_down_proj(X)))
         return X
